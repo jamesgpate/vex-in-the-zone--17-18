8e84a4eed156fc8a82e3b116163e5e41c6ab6d81
notjoms <jgp5232002@gmail.com>
2017-10-19 07:58:25 -0500

    Just using the built-in pid

--- v3/driving.c
+++ v3/driving.c
@@ -1,4 +1,4 @@
-#include "pid.c"
+
 /*
     Copyright (C) 2017 Quantum Robotics
 
@@ -16,14 +16,10 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-float pidRequestValue = 0;
 task drive(){
 	bool precision = false;
 	int c2 = 0;
 	int c3 = 0;
-	float pid_Kp = 2;
-	float pid_Ki = 1;
-	float pid_Kd = 0;
 	SensorValue[towerL] = 0;
 	SensorValue[towerR] = 0;
 	while(true){
@@ -54,25 +50,16 @@ task drive(){
 		//switch for above
 		if(vexRT[Btn8L]==1)
 			precision=!precision;
-		//Non-PID version
+		nMotorPIDSpeedCtrl[ldr4b] = mtrSpeedReg;
+		nMotorPIDSpeedCtrl[rdr4b] = mtrSpeedReg;
 		//6U/6D for dr4b, 5U for half speed
 
 		if(vexRT[Btn6U]==1){
-			if(vexRT[Btn5U]==1){
-				motor[ldr4b] = 63;
-				motor[rdr4b] = -63;
-			}else if(vexRT[Btn5U]==0){
 				motor[ldr4b] = 127;
 				motor[rdr4b] = -127;
-			}
 		}else if(vexRT[Btn6D]==1){
-			if(vexRT[Btn5U]==1){
-				motor[ldr4b] = -63;
-				motor[rdr4b] = 63;
-			}else if(vexRT[Btn5U]==0){
 				motor[ldr4b] = -127;
 				motor[rdr4b] = 127;
-			}
 		}else{
 			motor[ldr4b] = 0;
 			motor[rdr4b] = 0;
@@ -86,23 +73,7 @@ task drive(){
 		else
 			motor[claw] = 0;
 
-		//PID version
-/*
-		if(vexRT[Btn6U]==1){
-				pid(ldr4b,towerL,pid_Kp,pid_Ki,pid_Kd,pidRequestValue+=0.5);
-				pid(rdr4b,towerR,pid_Kp,pid_Ki,pid_Kd,-pidRequestValue);
-		}else if(vexRT[Btn6D]==1){
-				pid(ldr4b,towerL,pid_Kp,pid_Ki,pid_Kd,pidRequestValue-=0.5);
-				pid(rdr4b,towerR,pid_Kp,pid_Ki,pid_Kd,-pidRequestValue);
-		}
-		if(vexRT[Btn5U]==1){
-			motor[chain] = 127;
-		}else if (vexRT[Btn5D]==1){
-			motor[chain] = -127;
-		}else{
-			motor[chain] = 0;
-		}
-		*/
+
 		//sounds
 		if(!bSoundActive){
 			if(vexRT[Btn8R]==1){
