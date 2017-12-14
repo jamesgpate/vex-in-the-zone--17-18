ce3a83b6d05012baf458fb282132da8fa00a37b1
notjoms <jgp5232002@gmail.com>
2017-12-13 21:45:53 -0600

    auton changes, changed file structure, and tweaked driving a little

--- new/auton.c
+++ new/auton.c
@@ -15,31 +15,73 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-int ledCount = 0;
-int dOfWheels = 4;
-int rOfRobot = 8;
-float C_PI = 3.1415926;
+int lcdCount = 0;
+const int C_dOfWheels = 4;
+const int C_rOfRobot = 8;
+const float C_PI = 3.1415926;
+const int C_motorPower = 80;
 int getEncValForDistance(int inches){//this returns the encoder value for drivetrain distance
 	return (360*inches)/(dOfWheels*C_PI/2);
 }
 int getEncValForTurn(int degrees){//this returns how many times an encoder on the drivetrain needs to turn in relation to how far the robot needs to turn
-	return (360*PI*2*rOfRobot)/(360*dOfWheels);
+	return (360*C_PI*2*rOfRobot)/(360*dOfWheels);
 }
 void moveForwards(int distance){//this moves the robot forwards *distance* inches
-
+	int encVal = getEncValForDistance(distance);
+	SensorValue[ldtEnc] = 0;
+	SensorValue[rdtEnc] = 0;
+	while(SensorValue[ldtEnc]<encVal || SensorValue[rdtEnc]<encVal){
+		motor[ldt1] = motor[ldt2] = C_motorPower;
+		motor[rdt1] = motor[rdt2] = -C_motorPower;
+	}
+	motor[ldt1] = motor[ldt2] = 0;
+	motor[rdt1] = motor[rdt2] = 0;
 }
 void moveBackwards(int distance){//this moves the robot backwards *distance* inches
-
+	int encVal = getEncValForDistance(distance);
+	SensorValue[ldtEnc] = 0;
+	SensorValue[rdtEnc] = 0;
+	while(SensorValue[ldtEnc]>encVal || SensorValue[rdtEnc]>encVal){
+		motor[ldt1] = motor[ldt2] = -C_motorPower;
+		motor[rdt1] = motor[rdt2] = C_motorPower;
+	}
+	motor[ldt1] = motor[ldt2] = 0;
+	motor[rdt1] = motor[rdt2] = 0;
 }
 void turnRight(int degrees){//this turns the robot to the right *degrees* degrees
-
+	int encVal = getEncValForTurn(degrees);
+	SensorValue[ldtEnc] = 0;
+	SensorValue[rdtEnc] = 0;
+	while(SensorValue[ldtEnc]<encVal || SensorValue[rdtEnc]<encVal){
+		motor[ldt1] = motor[ldt2] = C_motorPower;
+		motor[rdt1] = motor[rdt2] = C_motorPower;
+	}
+	motor[ldt1] = motor[ldt2] = 0;
+	motor[rdt1] = motor[rdt2] = 0;
 }
 void turnLeft(int degrees){//this turns the robot to the left *degrees* degrees
-
+	int encVal = getEncValForTurn(degrees);
+	SensorValue[ldtEnc] = 0;
+	SensorValue[rdtEnc] = 0;
+	while(SensorValue[ldtEnc]>encVal || SensorValue[rdtEnc]>encVal){
+		motor[ldt1] = motor[ldt2] = -C_motorPower;
+		motor[rdt1] = motor[rdt2] = -C_motorPower;
+	}
+	motor[ldt1] = motor[ldt2] = 0;
+	motor[rdt1] = motor[rdt2] = 0;
 }
-
+void lowerMGM(){
+	motor[mgml] = motor[mgmr] = C_motorPower;
+	wait1Msec(500);
+	motor[mgml] = motor[mgmr] = 0;
+}
+void raiseMGM(){
+	motor[mgml] = motor[mgmr] = -C_motorPower;
+	wait1Msec(500);
+	motor[mgml] = motor[mgmr] = 0;
+}
 task auton(){//main task
-	switch(ledCount){
+	switch(lcdCount){
 		case 0://first auton
 			break;
 		case 1:
@@ -52,4 +94,3 @@ task auton(){//main task
 			break;
 	}
 }
-//mgm up = 4095 mgm down 2550
